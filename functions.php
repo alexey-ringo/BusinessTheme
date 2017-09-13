3633f3067a17b8b73233951b50ea7643d5eb504d
Alex Ringo <alexey.ringo@gmail.com>
2017-09-13 21:20:45 +0000

    Delete all unnecessary and add css and js files. Insert css code into style.css. Link they in functions.php

--- functions.php
+++ functions.php
@@ -16,23 +16,7 @@ if ( ! function_exists( 'html2wp_setup' ) ) :
 	 * as indicating support for post thumbnails.
 	 */
 	function html2wp_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Business, use a find and replace
-		 * to change 'html2wp' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'html2wp', get_template_directory() . '/languages' );
-
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
+		
 		add_theme_support( 'title-tag' );
 
 		/*
@@ -59,41 +43,12 @@ if ( ! function_exists( 'html2wp_setup' ) ) :
 			'caption',
 		) );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'html2wp_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
+		
+		
 	}
 endif;
 add_action( 'after_setup_theme', 'html2wp_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function html2wp_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'html2wp_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'html2wp_content_width', 0 );
 
 /**
  * Register widget area.
@@ -102,7 +57,7 @@ add_action( 'after_setup_theme', 'html2wp_content_width', 0 );
  */
 function html2wp_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'html2wp' ),
+		'name'          => 'Область виждетов',
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'html2wp' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
@@ -116,46 +71,32 @@ add_action( 'widgets_init', 'html2wp_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
+function html2wp_style() {
+	wp_enqueue_style( 'grid', get_template_directory_uri() . '/css/grid.css');
+	wp_enqueue_style( 'artbt-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'font', 'https://fonts.googleapis.com/css?family=Roboto:400,500,700');
+	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+	wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.css');
+	wp_enqueue_style( 'colorScheme', get_template_directory_uri() . '/css/colorScheme-2.css');
+	wp_enqueue_style( 'carousel', get_template_directory_uri() . '/css/owl-carousel.css');
+}
+add_action( 'wp_enqueue_scripts', 'html2wp_style' ); 
+ 
 function html2wp_scripts() {
-	wp_enqueue_style( 'html2wp-style', get_stylesheet_uri() );
-
+	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/script.js', array ('jquery'), '', true );
+	
+/**	
 	wp_enqueue_script( 'html2wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'html2wp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+*/
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'html2wp_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
+ 
 /**
  * Load WooCommerce compatibility file.
  */
