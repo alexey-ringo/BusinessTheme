96de6ad8fb5db28284976a5de38e8ce651c751ac
Alex Ringo <alexey.ringo@gmail.com>
2017-09-16 22:08:21 +0000

    Correction templates file 'content.php' - remove underscores default function

--- template/content_old.php
+++ template/content_old.php
@@ -1,4 +1,13 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Business
+ */
 
+?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
@@ -8,7 +17,13 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-		 ?>
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php html2wp_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -33,5 +48,7 @@
 		?>
 	</div><!-- .entry-content -->
 
-
+	<footer class="entry-footer">
+		<?php html2wp_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
