5bc6e05dd62df7aaa4a16583593b3fea17e168bb
Alex Ringo <alexey.ringo@gmail.com>
2017-09-16 21:58:12 +0000

    Preparing posts output - single.php

--- single.php
+++ single.php
@@ -9,13 +9,13 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
+<main id="main" class="site-main">
+	<div class="container">
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part( 'template/content', get_post_type() );
 
 			the_post_navigation();
 
@@ -26,9 +26,9 @@ get_header(); ?>
 
 		endwhile; // End of the loop.
 		?>
+	</div>
+</main><!-- #main -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php
 get_sidebar();
