ba54f1f0ed73b1d1670c119cb3db467adca18fa5
Alex Ringo <alexey.ringo@gmail.com>
2017-09-16 20:41:44 +0000

    check exist active sidebar before output_final

--- footer.php
+++ footer.php
@@ -27,14 +27,14 @@
               <div class="grid_4">
                 <?php 
                 if (is_active_sidebar('footer-center')) :
-                dynamic_sidebar('footer-left'); 
+                dynamic_sidebar('footer-center'); 
                 endif;
                 ?>
               </div>
               <div class="grid_4">
                 <?php 
                 if (is_active_sidebar('footer-right')) :
-                dynamic_sidebar('footer-left'); 
+                dynamic_sidebar('footer-right'); 
                 endif;
                 ?>
               </div>
